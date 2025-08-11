5f9294af0ba4543d672e1b4999fef8b61bb03843
arthur01Bandeira <arthurj.bandeira@gmail.com>
2025-08-11 20:06:22 -0300

    firstStep

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { Fragment } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './src/screens/Home';
+
+export default function App() {
+  return (
+    <Home />
+  ); 
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bottom: {
+    flex: 1,
+    backgroundColor : '#000000',
+    alignItems : 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '700',
+    color:'#000000'
+  }
+});
